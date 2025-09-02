08934f63cd1adc51ef170bfe1670c85d20275ddc
Fahad <78952725+Ranger-NF@users.noreply.github.com>
2025-09-02 11:14:07 +0530

    Initial Commit

---
+++ src/test/java/com/clevo/wastemanagement/WastemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clevo.wastemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WastemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
