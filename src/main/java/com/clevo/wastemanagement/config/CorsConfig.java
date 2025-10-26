60f1aa7f6c1c551eb17dbaee2ce7b6056905eb0e
Fahad <78952725+Ranger-NF@users.noreply.github.com>
2025-10-26 10:35:30 +0530

    refactor: Allowed all hosts to connect

--- src/main/java/com/clevo/wastemanagement/config/CorsConfig.java
+++ src/main/java/com/clevo/wastemanagement/config/CorsConfig.java
@@ -7,16 +7,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:8081") // frontend
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                registry
+                    .addMapping("/api/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
             }
         };
     }
